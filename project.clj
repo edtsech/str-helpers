e7b464515ececb9a828533fab509bb5ca205cf44
Edward Tsech <edtsech@gmail.com>
2012-08-05 21:59:25 +0200

    Fix desc.

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject str-helpers "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject str-helpers "0.1.0-SNAPSHOT"
+  :description "Few helper functions for Clojure strings."
   :dependencies [[org.clojure/clojure "1.3.0"]])
