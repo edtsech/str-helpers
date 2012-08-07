c72c05191c16cf290ed4b43bbd68afb9a0340bd2
Edward Tsech <edtsech@gmail.com>
2012-08-07 20:55:01 +0200

    Change version

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject str-helpers "0.1.0-SNAPSHOT"
+(defproject str-helpers "0.1.0"
   :description "Few helper functions for Clojure strings."
   :dependencies [[org.clojure/clojure "1.3.0"]])
